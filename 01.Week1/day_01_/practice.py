1307c0294b667b7c550a4d558ffa5af6e664bfd0
탁윤희 <gwalgwal98@naver.com>
2023-07-17 10:48:30 +0900

    01. 파이썬 기초

---
+++ 01.Week1/day_01_/practice.py
@@ -0,0 +1,28 @@
+a = 1
+b = 2 
+c = a + b
+
+
+# 변수명으로 직관적인 이름 사용 
+num1 = 1
+num2 = 2
+sum_result = num1 + num2 
+
+# 온도 예시
+temperature = 25
+is_hot = temperature > 30 # is: True, False
+
+# 자료 단수/복수형 구별
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+# 시간 예시 
+seconds = 60
+minutes = 60
+hours = 24 
+
+# 상수
+SECONDS = 60 # 대문자 변수: 고정값
+SECONDS_PER_MINUTE = 60
