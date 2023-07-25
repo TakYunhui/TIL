35f88e651f440da94122eedbd1dc54561b916dac
탁윤희 <gwalgwal98@naver.com>
2023-07-25 15:40:36 +0900

    problem e solved * 복습하기

--- 02.Week2/day_02_Data_Structure2/problems/problem_d.py
+++ 02.Week2/day_02_Data_Structure2/problems/problem_d.py
@@ -1,8 +1,16 @@
 # Q4. 혈액형 타입을 담은 blood_types리스트가 주어 질 때,
 # 각 혈액형 타입을 키로, 누적 수를 value로 하는 딕셔너리 blood를 만들어 반환하는
-# 함수 make_blood를 작성하시오.
+# 함수 make_blood를 작성하시오
+
 
 blood_types = ['A', 'B', 'O', 'AB', 'A', 'O', 'B', 'AB', 'A', 'B']
 
 # make_blood(blood_types) -> {'A': 3, 'B': 3, 'AB': 2, 'O': 2}
 
+def make_blood(blood_types):
+    blood = {}
+    for blood_type in blood_types:
+        blood[blood_type] = blood.get(blood_type, 0) + 1
+    print(blood)
+
+make_blood(blood_types)
