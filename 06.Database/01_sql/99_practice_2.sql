1bb3f256ce7f3362cde9af2cfe1df8ea9008e81e
탁윤희 <gwalgwal98@naver.com>
2023-10-10 18:00:39 +0900

    practice

--- 06.Database/01_sql/99_practice_2.sql
+++ 06.Database/01_sql/99_practice_2.sql
@@ -3,16 +3,42 @@
 
 
 -- Authors 테이블을 생성한다. 상세한 schema는 첨부 파일 참고.
+CREATE TABLE Authors (
+  name VARCHAR(50) NOT NULL,
+  nationality VARCHAR(50) NOT NULL
+);
 
 -- Books테이블을 생성한다. 상세한 schema는 첨부 파일 참고.
+CREATE TABLE Books (
+  title VARCHAR(50) NOT NULL,
+  pub_year INTEGER NOT NULL,
+  author_id INTEGER NOT NULL
+);
 
 -- Authors 테이블에 서로 다른 데이터를 최소 3개 이상 삽입한다.
+DROP TABLE Authors;
+INSERT INTO Authors
+  (name, nationality)
+VALUES 
+  ('소정', '대한민국'),
+  ('지연', '샌프란시스코'),
+  ('베니', '대한민국');
 
 -- Books 테이블에 서로 다른 데이터를 최소 3개 이상 삽입한다.
-
+DROP TABLE Books;
+INSERT INTO Books
+  (title, pub_year, author_id)
+VALUES
+  ('떡볶이', 1995, 1),
+  ('감자탕', 1998, 2),
+  ('피자', 1997, 3);
 -- Authors 테이블의 모든 데이터 조회한다.
+SELECT * FROM Authors;
 
 -- Books 테이블의 모든 데이터 조회한다. 단, 관계 맺고 있는 Authors 테이블의 name도 함께 출력한다.
+SELECT * FROM Books
+INNER JOIN Authors
+  ON Authors.name = Books.title;
 
 -- Authors 테이블의 1번째 레코드의 nationality를 '스코틀랜드'로 수정한다.
 
