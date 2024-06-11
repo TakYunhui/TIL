08df7ab9b01a1ece9e9981af2cb51878a72b5e90
탁윤희 <gwalgwal98@naver.com>
2024-06-11 17:13:54 +0900

    노마드코더: next.js loading component

--- nomad_coder/next_js/app/(home)/page.tsx
+++ nomad_coder/next_js/app/(home)/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-    await new Promise((resolve) => setTimeout(resolve, 5000)); // getMovies 함수가 여기서 일단 멈춤
+    await new Promise((resolve) => setTimeout(resolve, 10000)); // getMovies 함수가 여기서 일단 멈춤
     const response = await fetch(URL);
     const json = await response.json();
     return json;
