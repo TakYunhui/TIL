b0e23ed36709c75422a451bd52fab49f77d3b84e
탁윤희 <gwalgwal98@naver.com>
2024-06-11 17:36:07 +0900

    노마드코더: await Promise.all을 이용한 데이터 fetching 병렬 처리(동시 작동)

--- nomad_coder/next_js/app/(home)/page.tsx
+++ nomad_coder/next_js/app/(home)/page.tsx
@@ -1,22 +1,31 @@
+import Link from "next/link";
 import { resolve } from "path";
 
 export const metadata = {
     title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-    await new Promise((resolve) => setTimeout(resolve, 10000)); // getMovies 함수가 여기서 일단 멈춤
-    const response = await fetch(URL);
+    // await new Promise((resolve) => setTimeout(resolve, 1000)); // getMovies 함수가 여기서 일단 멈춤
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
 
+
 export default async function HomePage() {
     const movies = await getMovies();
-    
-    return <div>
-        {JSON.stringify(movies)}
-    </div>;
+
+    return (<div>
+        {movies.map((movie) =>
+            <li key={movie.id}>
+                <Link href={`/movies/${movie.id}`}>
+                    {movie.title}
+                </Link>
+            </li>)
+        }
+    </div>
+    );
 }
--- nomad_coder/next_js/app/(movies)/movies/[id]/page.tsx
+++ nomad_coder/next_js/app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,21 @@
-export default function MovieDetail(
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id:string) {
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id:string) {
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail(
     {params: {id}, 
 }: {
     params: {id: string}
 } 
 ) {
-    return <h1> Movie {id}</h1>
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+    return <h1> Movie {movie.title}</h1>
 }
