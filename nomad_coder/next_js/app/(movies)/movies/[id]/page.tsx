ab9125b47f0c148cd44fd99c6ad4fcabc46bd127
탁윤희 <gwalgwal98@naver.com>
2024-06-11 18:27:29 +0900

    노마드코더: Suspense를 이용한 data fetching 동시 처리, UI 부분 표시 가능

--- nomad_coder/next_js/app/(movies)/movies/[id]/page.tsx
+++ nomad_coder/next_js/app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,6 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id:string) {
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
-
-async function getVideos(id:string) {
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
-}
+import { Suspense } from "react"
+import MovieInfo from "../../../../components/movie-info"
+import MovieVideos from "../../../../components/movie-videos"
 
 export default async function MovieDetail(
     {params: {id}, 
@@ -16,6 +8,14 @@ export default async function MovieDetail(
     params: {id: string}
 } 
 ) {
-    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-    return <h1> Movie {movie.title}</h1>
+
+    return (
+    <div>
+        <Suspense fallback={<h1>Loading movie info...</h1>}>
+        <MovieInfo id={id} />
+        </Suspense>
+        <Suspense fallback={<h1>Loading movie videos...</h1>}>
+        <MovieVideos id={id}/>
+        </Suspense>
+    </div>)
 }
