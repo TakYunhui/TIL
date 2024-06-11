c9cc9f5999d03db5315de9156e988df89723511d
탁윤희 <gwalgwal98@naver.com>
2024-06-11 17:08:00 +0900

    노마드코더: server side data fetching

--- nomad_coder/next_js/app/(home)/page.tsx
+++ nomad_coder/next_js/app/(home)/page.tsx
@@ -1,9 +1,22 @@
+import { resolve } from "path";
+
 export const metadata = {
     title: "Home",
 };
 
-export default function Tomato() {
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // getMovies 함수가 여기서 일단 멈춤
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
+    
     return <div>
-        <h1>Hello!</h1>
+        {JSON.stringify(movies)}
     </div>;
 }
